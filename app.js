20d24a20f5e2a09ed24af130eb2a73f08326ce17
sk569ppups <skato.toay@gmail.com>
2025-09-23 23:19:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ function saveAs(filename, text){
 
 }
 
-}
 // ===== pdf.js ローダ（グローバル未定義なら動的に読み込む） =====
 
   if (window.pdfjsLib) return;
@@ -283,6 +282,7 @@ function filterCandidateLines(lines){
 }
 
 })();
+
 
 
 
